d60289205280380c7d051ba43973c60a25d0e0bb
megganeturner <megganeturner@gmail.com>
2017-02-14 16:54:54 +1100

    destroying friends (not yet working)

--- app/routes/friends/show.js
+++ app/routes/friends/show.js
@@ -1,4 +1,12 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  actions: {
+    delete(friend) {
+      console.log('trying to delete: ', friend)
+      friend.destroyRecord().then(() => {
+        this.transitionToRoute('friends.index');
+      })
+    }
+  }
 });
