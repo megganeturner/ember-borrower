d975e377d7cc0662d085335c46116c59424feba3
megganeturner <megganeturner@gmail.com>
2017-02-14 15:33:38 +1100

    add friend

--- app/components/friends/edit-form.js
+++ app/components/friends/edit-form.js
@@ -1,4 +1,36 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  isValid: Ember.computed(
+    'model.email',
+    'model.firstName',
+    'model.lastName',
+    'model.twitter',
+    {
+      get() {
+        return !Ember.isEmpty(this.get('model.email')) &&
+               !Ember.isEmpty(this.get('model.firstName')) &&
+               !Ember.isEmpty(this.get('model.lastName')) &&
+               !Ember.isEmpty(this.get('model.twitter'));
+      }
+    }
+  ),
+  actions: {
+    save() {
+      console.log('+- save action in edit-form component');
+      if (this.get('isValid')) {
+        this.get('model').save().then((friend) => {
+          return this.save(friend);
+        }, (err) => {
+          this.set('errorMessage', 'there was something wrong saving the model');
+        });
+      } else {
+        this.set('errorMessage', 'You have to fill all the fields');
+      }
+    },
+    cancel() {
+      console.log('+- cancel action in edit form component');
+      this.cancel();
+    }
+  }
 });
