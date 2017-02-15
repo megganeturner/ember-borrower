6d4db6f35a01e8ebaff0692ebb62df9744a19ab5
megganeturner <megganeturner@gmail.com>
2017-02-15 15:54:16 +1100

    loans route & relationship

--- app/models/friend.js
+++ app/models/friend.js
@@ -5,6 +5,7 @@ export default DS.Model.extend({
   lastName: DS.attr('string'),
   email: DS.attr('string'),
   twitter: DS.attr('string'),
+  loans: DS.hasMany('loan'),
   fullName: Ember.computed('firstName', 'lastName', {
     get() {
       return this.get('firstName') + ' ' + this.get('lastName');
